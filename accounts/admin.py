c3ca994cd26f6383e63791c59a6e526b2f3d9593
Divya <dd7735811@gmail.com>
2026-04-04 15:59:10 +0530

    custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = [
+        "email",
+        "username",
+        "age",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields" : ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields" : ("age",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+class TestUserManager(TestCase):
+    def testusermodel(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = "testuser",
+            email = "testuser@example.com",
+            password = "testpass123"
+            )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def testsuperusermodel(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username= "testsuperuser",
+            email="testsuperuser@example.com",
+            password="testsuperuser123"
+        )
+
+        self.assertEqual(admin_user.username, "testsuperuser")
+        self.assertEqual(admin_user.email, "testsuperuser@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
