c9c43cb63f0cbe1b5a6400b1a67e23da7d2a0fbd
Divya <dd7735811@gmail.com>
2026-04-07 18:48:31 +0530

    render deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "news-8u3v.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -144,4 +144,4 @@ CRSIPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 TIME_ZONE = "Asia/Kolkata"
-CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://news-8u3v.onrender.com"]
